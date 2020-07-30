18af1b1e50f0dfa62d9298d3178a6f7a24e843e3
imac <imac@imacnoiMac.local>
2020-07-31 01:01:54 +0900

    アクセス制限の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user,{only: [:new,:edit,:create,:update,:destroy]}
+
   def show
     @post = Post.find_by(id: params[:id])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+
   def index
     @users = User.all.order(created_at: :desc)
   end
@@ -19,6 +23,7 @@ class UsersController < ApplicationController
       image_name: "default_user.jpg"
     )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -56,6 +61,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email],password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to('/')
     else
@@ -63,6 +69,19 @@ class UsersController < ApplicationController
       @email = params[:email]
       @password = params[:password]
       render('users/login_form')
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = 'ログアウトしました'
+    redirect_to('/login')
+  end
+
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/")
     end
   end
 
