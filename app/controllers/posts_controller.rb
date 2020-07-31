4e8503166c5804436becd792d34172a2042c72c9
imac <imac@imacnoiMac.local>
2020-07-31 11:55:06 +0900

    いいね機能、ユーザーページでお気に入り記事の一覧表示機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
-    @user = User.find_by(id: @post.user_id)
+    @user = @post.user
+    @likes_count = Like.where(post_id: @post.id).count
   end
 
   def new
